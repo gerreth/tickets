9b893649244bde1ecb5fee353f8a09df9256887e
Gerret Halberstadt <gerrethalberstadt@Gerrets-iMac.fritz.box>
2018-03-30 12:53:40 +0200

    Initial commit

---
+++ src/lib/hello_web/views/ticket_view.ex
@@ -0,0 +1,3 @@
+defmodule HelloWeb.TicketView do
+  use HelloWeb, :view
+end
---
+++ src/priv/repo/migrations/20180329083208_create_users.exs
@@ -0,0 +1,16 @@
+defmodule Hello.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+
+      add :username, :string
+      add :email, :string
+      add :password, :string
+
+      timestamps()
+    end
+
+  end
+
+end
